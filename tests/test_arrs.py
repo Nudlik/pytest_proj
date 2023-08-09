836ae4054ddb9dd9dad963cab15798b47cb56fc4
Nudlik <gegorp@mail.com>
2023-08-09 19:11:01 +0700

    refactor funcs add parametrize

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,14 +1,22 @@
+import pytest
+
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], -1, "test") == "test"
+@pytest.mark.parametrize('array, index, default, expected', [
+    ([1, 2, 3], 1, "test", 2),
+    ([], -1, "test", "test")
+])
+def test_get(array, index, default, expected):
+    assert arrs.get(array, index, default) == expected
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([], 1) == []
-    assert arrs.my_slice([1], -1) == [1]
-    assert arrs.my_slice([1], -2) == [1]
+@pytest.mark.parametrize('coll, start, end, expected', [
+    ([1, 2, 3, 4], 1, 3, [2, 3]),
+    ([1, 2, 3], 1, None, [2, 3]),
+    ([], 1, None, []),
+    ([1], -1, None, [1]),
+    ([1], -2, None, [1]),
+])
+def test_slice(coll, start, end, expected):
+    assert arrs.my_slice(coll, start, end) == expected
